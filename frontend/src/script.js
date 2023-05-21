01c77e70e428b3178094ffcff3240644f92a4ac8
EJT93 <elijahtorrence+github1@gmail.com>
2023-05-21 14:01:10 -0400

    fixed <genre> bug in js file.

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -9,7 +9,7 @@ function generateStory() {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            genre: selectedGenre, 
+            genre: genre, 
         }),
     })
     .then(response => response.json())
